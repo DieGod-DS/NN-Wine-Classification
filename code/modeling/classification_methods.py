2ad3384b7db8971fa44092a3d50f29fda938c46e
Diego Armando Teles de Carvalho <127405269+DieGod69@users.noreply.github.com>
2024-01-04 22:23:57 -0300

    04/01/24.3

--- code/modeling/classification_methods.py
+++ code/modeling/classification_methods.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 
 warnings.filterwarnings('ignore')
 
-class Classification():
+class Classification_ann():
     
     
     def __init__(self):
@@ -26,3 +26,51 @@ class Classification():
         return data
     
     
+    def props(self, df, target, y_train, y_val, y_test):
+        '''
+        esta função retorna as proporções das classes
+        '''
+        props = pd.DataFrame(df[target].value_counts(normalize=True).values,
+                             index = df[target].value_counts(normalize=True).index,
+                             columns = ['original'])
+        
+        props['treino'] = y_train[target].value_counts(normalize=True).values
+        props['val'] = y_val[target].value_counts(normalize=True).values
+        props['teste'] = y_test[target].value_counts(normalize=True).values
+        
+        return props
+    
+    
+    def create_model(x, n_neurons:list):
+        '''
+        esta função cria uma rede neural
+        '''
+        model = Sequential()
+        
+        if len(n_neurons) == 1:
+            model.add(Dense(n_neurons[0], input_shape=(11,), activation='relu'))
+        else:
+            for index, n in n_neurons:
+                if index == 0:
+                    model.add(Dense(n, input_shape=(11,), activation='relu'))
+                else:
+                    model.add(Dense(n, activation='relu'))
+                    
+        model.add(Dense(1, activation='sigmoid'))
+    
+        return model
+    
+    
+    def scal_data(self, x_train, x_val, x_test):
+        '''
+        esta função normaliza os dados
+        '''
+        
+        scaler = MinMaxScaler(feature_range=(0,1))
+        scaler.fit(x_train)
+        
+        x_train_norm = scaler.transform(x_train)
+        x_val_norm = scaler.transform(x_val)
+        x_test_norm = scaler.transform(x_test)
+        
+        return x_train_norm, x_val_norm, x_test_norm
